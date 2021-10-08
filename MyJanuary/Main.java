3c8fd4f5717dd038955e4b3567d27f3af9aa8c7a
MyJanuary <89083186+MyJanuary@users.noreply.github.com>
2021-10-08 18:33:44 +0900

    #10845
    배열을 이용하는 방법 사용.
    큐 라이브러리 사용하는 방법으로도 한 번 해보겠음.

--- MyJanuary/Main.java
+++ MyJanuary/Main.java
@@ -1,26 +1,110 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+class Queue{ // Queue 클래스
+    public int[] queue; // 데이터를 담을 큐 선언
+    public int f_pointer = 0; // 큐의 앞을 가리킬 함수
+    public int b_pointer = -1; // 큐의 뒤를 가리킬 함수
+	
+	Queue(){ // 기본 클래스
+		
+	}
+	
+	Queue(int number){
+		queue = new int[number];
+	}
+	
+	// 데이터 삽입
+	public void push(int x) {
+		queue[++b_pointer] = x;
+	}
+	
+	// 데이터 삭제
+	public int  pop() {
+		if (empty() == 0)
+			return -1;
+		else
+			return queue[f_pointer++];
+	}
+	
+	// 데이터 개수
+	public int size() {
+		return b_pointer - f_pointer + 1;
+	}
+	
+	// 데이터 여부
+	public int empty() {
+		if (size()==0)
+			return 1;
+		else
+			return 0;
+	}
+	
+
+	// 가장 앞의 데이터
+	public int front() {
+		if (empty() == 0)
+			return -1;
+		else
+			return queue[f_pointer];
+	}
+	
+	// 가장 뒤의 데이터
+	public int back() {
+		if (empty() == 0)
+			return -1;
+		else
+			return queue[b_pointer];
+	}
+} // Queue 클래스 종료
+
 public class Main { // 클래스 시작
-    public static void main(String[] args) { // 메인 클래스 시작
-        int[] f = new int[10001]; // 배열 생성 (<= 10,000) 
-        
-        for (int i = 1; i <= 10000; i++) { // for문 시작
-            if(d(i) < 10001){ // 셀프 넘버가 10,001 보다 작으면
-                f[d(i)] = 1;
-            }
-        } // for문 끝
-        for (int i = 1; i <= 10000; i++) { // for문 시작
-            if(f[i] != 1){ // 배열의 개수가 1이 아니라면 
-                System.out.println(i); // 출력
-            }
-        }// for문 끝
-    } // 메인 클래스 끝
     
-    public static int d(int i) {
-        int sum = i; // 전부 더한 수
-        if (i >= 10000) { sum += i / 10000; i %= 10000; } // 첫째 자리수
-        if (i >= 1000) { sum += i / 1000; i %= 1000; } // 둘째 자리수
-        if (i >= 100) { sum += i / 100; i %= 100; } // 셋째 자리수
-        if (i >= 10) { sum += i / 10; i %= 10; } // 넷째 자리수
-        
-        return sum += i;
-    }
-} // 클래스 끝
+	public static void main(String[] args) { // 메인 클래스 시작
+	    Scanner s = new Scanner(System.in);
+	    
+		int number = s.nextInt(); // 데이터의 저장 공간 확보
+		
+		Queue queue = new Queue(number);
+		
+		for(int i = 0 ; i < number ; i++){ // cnt 만큼 반복할 수 있도록 for문 사용
+		    String a = s.next();
+		    
+		    switch(a){ // switch-case문 시작
+		    case "push":
+		    	int x = s.nextInt();
+				queue.push(x);
+				break;
+                
+			case "pop":
+				System.out.println(queue.pop());
+				break;
+                
+			case "size":
+				System.out.println(queue.size());
+				break;
+                
+			case "empty":
+				System.out.println(queue.empty());
+				break;
+                
+			case "front":
+				System.out.println(queue.front());
+				break;
+			
+			case "back":
+				System.out.println(queue.back());
+				break;
+		    } // switch-case문 종료
+		} // for문 종료
+	} //메인 클래스 종료
+}//클래스 종료
+
+ 
