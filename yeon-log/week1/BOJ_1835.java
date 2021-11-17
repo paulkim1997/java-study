bf7caaa77563925696de7d45b4f0e20aa2d0e9c3
Jiyeon <88316548+yeon-log@users.noreply.github.com>
2021-11-18 01:25:11 +0900

    카드

--- yeon-log/week1/BOJ_1835.java
+++ yeon-log/week1/BOJ_1835.java
@@ -14,17 +14,14 @@ public class BOJ_1835 {
         int N = Integer.parseInt(br.readLine()); // 카드의 개수
         //int[] desk = new int[N];
 
-        while (N != 0) {
+        while (N-- > 0) {
             if(card.isEmpty()) card.offerFirst(N); // N 카드를 제일 먼저 책상에 둔다
-            N--;
             int cnt = N;
-            if(N==0) break;
-            while (cnt != 0) {
+            while (cnt-- > 0) {
                 if(!card.contains(N)) card.offerFirst(N); // 같은 숫자 카드 추가는 한번만
                 int temp = card.peekLast(); // 맨 뒤에 카드를 기록해놓고
                 card.pollLast(); // 맨 뒤 카드 삭제하고
                 card.offerFirst(temp); // 맨 앞에 그 카드 숫자 추가
-                cnt--;
             }
         }
         for (Integer c : card) {
